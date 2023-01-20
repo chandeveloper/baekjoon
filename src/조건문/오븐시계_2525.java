d7d32b2cff24b153a0cdce86174826c3eea87ea6
Chan Park <chandeveloper122@gmail.com>
2023-01-20 17:34:28 +0900

    baekjoon_2525

---
+++ src/조건문/오븐시계_2525.java
@@ -0,0 +1,23 @@
+package 조건문;
+
+import java.util.Scanner;
+
+public class 오븐시계_2525 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		
+		int min = a * 60 + b;
+		min = min + c;
+		
+		int hour = (min / 60) % 24;	
+		int minute = min % 60;
+		
+		System.out.println(hour + " " + minute);
+	}
+}
