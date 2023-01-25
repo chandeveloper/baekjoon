213cf7100027be51f664eb4181dad2fef7acec72
Chan Park <chandeveloper122@gmail.com>
2023-01-25 15:16:49 +0900

    baekjoon_10950

---
+++ src/반복문/AB3_10950.java
@@ -0,0 +1,19 @@
+package 반복문;
+
+import java.util.Scanner;
+
+public class AB3_10950 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+
+		for (int i = 0; i < n; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			System.out.println(a + b);
+		}
+	}
+}
