86704ec6f83b8a4e9e63fd9e1fb4001b58a45967
Chan Park <chandeveloper122@gmail.com>
2023-01-25 16:47:33 +0900

    baekjoon_11021

---
+++ src/반복문/AB7_11021.java
@@ -0,0 +1,20 @@
+package 반복문;
+
+import java.util.Scanner;
+
+public class AB7_11021 {
+
+public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		
+		for (int i = 1; i <= t; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			System.out.println("Case #"+i+": "+ (a+b));
+		}
+	
+	}
+}
