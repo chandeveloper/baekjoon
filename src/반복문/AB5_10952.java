2cea0b90806f977c078beba22f6bd9432e80d7f9
Chan Park <chandeveloper122@gmail.com>
2023-01-25 17:55:10 +0900

    baekjoon_10952

---
+++ src/반복문/AB5_10952.java
@@ -0,0 +1,30 @@
+package 반복문;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class AB5_10952 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		while (true) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int sum = a+b;
+			if (a == 0 && b == 0) {
+				break;
+			}
+			bw.write(sum + "\n");
+		}
+	bw.flush();
+	bw.close();
+	}
+}
--- src/반복문/별찍기2_2439.java
+++ src/반복문/별찍기2_2439.java
@@ -7,7 +7,6 @@ public class 별찍기2_2439 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		sc.close();
 		
 		for (int i = 1; i <= n; ++i) {
 			for (int j = 1; j <= n - i; ++j) {
@@ -19,5 +18,6 @@ public class 별찍기2_2439 {
 			}
 			System.out.println();
 		}
+		sc.close();
 	}
 }
