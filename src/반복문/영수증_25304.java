ba6d337a3b84deeba7d98691b3d3f30ee53be5f3
Chan Park <chandeveloper122@gmail.com>
2023-01-25 15:34:36 +0900

    baekjoon_25304

---
+++ src/반복문/영수증_25304.java
@@ -0,0 +1,27 @@
+package 반복문;
+
+import java.util.Scanner;
+
+public class 영수증_25304 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int totalprice = sc.nextInt();
+		int totalcount = sc.nextInt();
+		
+		int total = 0;
+		
+		for (int i = 0; i < totalcount; i++) {
+			int price = sc.nextInt();
+			int num = sc.nextInt();
+			total += price*num;
+			}
+		if(total == totalprice) {
+				System.out.println("yes");
+			}else {
+				System.out.println("no");
+			}
+	sc.close();
+	}
+}
