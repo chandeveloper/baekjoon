b14ea5e71ded8898c981e5e244e93ec34a75e021
Chan Park <chandeveloper122@gmail.com>
2023-01-27 15:10:55 +0900

    baekjoon_10951

---
+++ src/반복문/AB4_10951.java
@@ -0,0 +1,20 @@
+package 반복문;
+
+import java.util.Scanner;
+
+public class AB4_10951 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		while (sc.hasNextInt()) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			
+			System.out.println(a + b);
+			
+		}
+	sc.close();
+	}
+}
